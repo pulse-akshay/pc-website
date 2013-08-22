aef77f8b90ecc5ed0db5ef7e592e404ef0882814
Akshay <tandon.akshay@gmail.com>
2013-08-22 05:22:53 +0200

    Initial Commit

---
+++ web.js
@@ -0,0 +1,3 @@
+//This file is the main file to get the application started on heroku
+//not sure if we will end up using it actually because we will need to know which server has already
+//been used to get the website running
